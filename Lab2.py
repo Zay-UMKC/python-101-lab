1478043f93143a59444c4ef19ef8fb40eac85d7d
Zay-UMKC <113187298+Zay-UMKC@users.noreply.github.com>
2022-09-09 09:46:28 -0500

    Add files via upload

--- Lab2.py
+++ Lab2.py
@@ -1,20 +1,21 @@
 print('**** Welcome to the LAB grade calculator! ****')
-#allows for someone to input the name of person and numbers to be calculated
+#allows for someone to input the name of person 
 name = input('Who are you calculating grades for? ==> ')
+#asking user about grades and attendance and letting them know if its between the acceptable range
 Lab_grade = float(input('Enter the labs grade? ==> '))
-Exam_grade = float(input('Enter the Exams grade? ==> '))
-Attend_grade = float(input('Enter the Attendance grade? ==> '))
 if Lab_grade > 100:
     Lab_grade = 100
     print('The Lab value should have been 100 or less.It has been changed to 100.')
 if Lab_grade < 0:
     print('The lab value should have been zero or greater. it had been changed to zero')
+Exam_grade = float(input('Enter the Exams grade? ==> '))
 if Exam_grade < 0: 
     Exam_grade = 0
     print('The Exam value should have been zero or greater. it had been changed to zero')
 if Exam_grade > 100:
     Exam_grade = 100
     print('The Exam value should have been 100 or less.It has been changed to 100.')
+Attend_grade = float(input('Enter the Attendance grade? ==> '))
 if Attend_grade < 0:
     Attend_grade = 0
     print('The Exam value should have been zero or greater. it had been changed to zero')
