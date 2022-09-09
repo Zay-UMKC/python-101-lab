b42f5f75b0788213be04a01816e78db19b391b51
Zay-UMKC <113187298+Zay-UMKC@users.noreply.github.com>
2022-09-09 09:37:35 -0500

    Add files via upload

--- Lab2.py
+++ Lab2.py
@@ -4,20 +4,38 @@ name = input('Who are you calculating grades for? ==> ')
 Lab_grade = float(input('Enter the labs grade? ==> '))
 Exam_grade = float(input('Enter the Exams grade? ==> '))
 Attend_grade = float(input('Enter the Attendance grade? ==> '))
+if Lab_grade > 100:
+    Lab_grade = 100
+    print('The Lab value should have been 100 or less.It has been changed to 100.')
+if Lab_grade < 0:
+    print('The lab value should have been zero or greater. it had been changed to zero')
+if Exam_grade < 0: 
+    Exam_grade = 0
+    print('The Exam value should have been zero or greater. it had been changed to zero')
+if Exam_grade > 100:
+    Exam_grade = 100
+    print('The Exam value should have been 100 or less.It has been changed to 100.')
+if Attend_grade < 0:
+    Attend_grade = 0
+    print('The Exam value should have been zero or greater. it had been changed to zero')
+if Attend_grade > 100:
+    Attend_grade = 100
+    print('The Attendance value should have been 100 or less.It has been changed to 100.')   
 #line for calculations of users inputs for grades
 gpa = round((Lab_grade * 0.7) + (Exam_grade * 0.2) + (Attend_grade * .1),2)
 #Output of the GPA
 print(f'The weighted grade for {name} is {gpa}')
 #Finds what his grade will be depending on his Gpa avg
-if total <= 100:
+if gpa <= 100:
     letter = 'A'
-if total <=89:
+if gpa <=89:
     letter = 'B'
-if total <=79:
+if gpa <=79:
     letter = 'C'
-if total <= 69:
+if gpa <= 69:
     letter = 'D'
-if total <=59:
+if gpa <=59:
     letter = 'F'
 #Output of the letter grade for users Gpa
 print(f'{name} has a letter grade of {letter}')
+print('**** Thanks for using the Lab grade calculator ****') 
